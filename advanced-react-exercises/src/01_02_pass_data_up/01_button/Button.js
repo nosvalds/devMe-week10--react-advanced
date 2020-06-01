fdcd8d8f0eb77d7fa6d99d70499202c3293e21bf
Nik Osvalds <nosvalds@gmail.com>
2020-06-01 15:16:19 +0100

    01 01 01 Button complete

--- advanced-react-exercises/src/01_02_pass_data_up/01_button/Button.js
+++ advanced-react-exercises/src/01_02_pass_data_up/01_button/Button.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 class Button extends Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            clicks: 0
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        const { handleUpdate } = this.props;
+        handleUpdate(this.state.clicks);
+
+        this.setState({
+            clicks: this.state.clicks + 1
+        })
+    }
+    
     render() {
+        const { clicks } = this.state;
 
-        return (<h1>Hello</h1>)
+        return (
+            <button 
+                className="btn btn-primary m-4"
+                onClick={ this.handleClick }
+            >
+                CLICK TRACKER: { clicks }
+            </button>
+            )
     }
 }
 
