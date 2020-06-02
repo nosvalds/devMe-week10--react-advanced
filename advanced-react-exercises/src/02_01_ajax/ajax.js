97a920266e6a6ecd8df3ae8ca240e6dd223c9651
Nik Osvalds <nosvalds@gmail.com>
2020-06-02 14:26:11 +0100

    02 01 ajax 06 - add comment to an article complete

--- advanced-react-exercises/src/02_01_ajax/ajax.js
+++ advanced-react-exercises/src/02_01_ajax/ajax.js
@@ -19,4 +19,11 @@ axios.patch("/articles/54", {
 }).then(({ data }) => console.log(data.data["tags"]));
 
 // Exercise 05 - Delete an article and console.log "Deleted"
-axios.delete("articles/56").then(console.log("Deleted"));
+axios.delete("articles/58").then(console.log("Deleted"));
+
+// Exercise 06 - Add a comment to an article and console.log() it's ID
+axios.post("/articles/54/comments", {
+	email: "ajaxSatan@hell.com",
+	comment: "True Axios"
+}).then(({ data }) => console.log(data.data["id"]));
+
