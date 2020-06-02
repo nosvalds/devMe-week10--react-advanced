29ae3440f5a0bf58a1c82fad6acd39a161700d1b
Nik Osvalds <nosvalds@gmail.com>
2020-06-02 14:08:30 +0100

    02 01 ajax 01 - get articles complete:

--- advanced-react-exercises/src/02_01_ajax/ajax.js
+++ advanced-react-exercises/src/02_01_ajax/ajax.js
@@ -1,9 +1,11 @@
 import axios from './axios';
 
-axios.get("/articles");
+// Exercise 01
+axios.get("/articles").then(({ data }) => console.log(data.data));
 
-axious.post("/articles", {
-    title: "Hello",
-    content: "Blah Blah Blah",
-    tags: ['fish', 'sticks'],
-});
+// Exercise 02
+// axious.post("/articles", {
+//     title: "Hello",
+//     content: "Blah Blah Blah",
+//     tags: ['fish', 'sticks'],
+// });
