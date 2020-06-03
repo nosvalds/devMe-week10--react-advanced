7bcb3833023c9ed977dbc444a6cbbb35eac3fbfd
Nik Osvalds <nosvalds@gmail.com>
2020-06-03 16:14:59 +0100

    update comments

--- advanced-react-exercises/src/hooks/Clicked.js
+++ advanced-react-exercises/src/hooks/Clicked.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 
 const Clicked = () => {
-    const [clicked, setClicked] = useState(false);
+    const [clicked, setClicked] = useState(false); // set up state with clicked initially false
 
-    const handleClick = () => setClicked(!clicked);
+    const handleClick = () => setClicked(!clicked); // flip boolean when handleClick is invoked
 
     return (
         <p
             onClick={ handleClick }
         >
-            {clicked ? "Clicked!" : "Not Clicked"}
+            {clicked ? "Clicked!" : "Not Clicked"} {/* Use ternary to flip between Clicked/Not Clicked based on value of clicked in state */}
         </p>
     );
 }
